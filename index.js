776a9c793bf20aa42b6d66ce74a3b0b6b9924c4d
jake-rigby <jak3r16by@gmail.com>
2017-06-24 14:45:25 +0100

    tidy up

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
 let Server = require('./src/js/server.js');
 let config = require('./config.json');
-
 let server = new Server(config);
--- src/js/server.js
+++ src/js/server.js
@@ -3,20 +3,13 @@ let express = require('express');
 // let mongoose = require('mongoose');
 
 module.exports = class Server {
+
 	constructor(config) {
 
-		this.config         = config;
-
-		// config.db.uri		= process.env.MONGODB_DB_URI			|| config.db.uri;
-		config.server.port 	= process.env.PORT 						|| config.server.port;
+		this.config = config;
+		config.server.port 	= process.env.PORT || config.server.port;
 
 		this.app = express();
-		// mongoose.connect(config.db.uri);
-		// this.models = require('./models')(mongoose);
-		// require('./auth')(config, app, mongoose, models.User);
-		// var userRouter = express.Router();
-		// app.use('/u', userRouter)
-		// require('./api')(userRouter, models)
 		this.app.use(express.static('www'));
 
 		this.app.listen(config.server.port, function () {
